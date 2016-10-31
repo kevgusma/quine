52bda310e828b4b57a24a01e6ca67ad4833caa55
Kevin GUSMAND <kevgusma@e1r1p6.42.fr>
2016-10-31 23:29:32 +0100

    finish

--- Sully.c
+++ Sully.c
@@ -1,12 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 #define MODE "w+"
-int main(){int i = 5;char*str;
-  char*a="#include<stdio.h>%c#include<stdlib.h>%c#define MODE %cw+%c%cint main(){int i = %d;char*a=%c%s%c;
-    fprintf(fopen(%cSully_%d.c%c,MODE),a,10,10,34,34,10,i,34,a,34,34,i-1,34,34,i-1,i-1,34,34,i-1,34,10);
-    system(%cclang -Wall -Wextra -Werror Sully_%d.c -o Sully_%d%c);
-    if(i > 0){system(%c./Sully_%d%c);}}%c";
-  sprintf(str, "Sully_%d", i);
-  fprintf(fopen(str,MODE),a,10,10,34,34,10,i,34,a,34,34,i-1,34,34,i-1,i-1,34,34,i-1,34,10);
-  sprintf(str, "clang -Wall -Wextra -Werror Sully_5.c -o Sully_5", i, i);
-  system(str);if(i > 0){sprintf(str, "Sully_%d", i);system(str);}}
+#define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
+int main(void){FILE *fd;
+int i = 5;
+char str[50];char*a="#include<stdio.h>%c#include<stdlib.h>%c#include<string.h>%c#define MODE %cw+%c%c#define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)%cint main(void){FILE *fd;%cint i = %d;%cchar str[50];char*a=%c%s%c;if (strcmp(__FILENAME__, %cSully.c%c)){i--;}sprintf(str, %cSully_%%d.c%c, i);fd = fopen(str, MODE);fprintf(fd,a,10,10,10,34,34,10,10,10,i,10,34,a,34,34,34,34,34,34,34,34,34,10);fclose(fd);char cmd[50];sprintf(cmd, %cclang -Wall -Wextra -Werror Sully_%%d.c -o Sully_%%d%c, i, i);system(cmd);if(i > 0){sprintf(str, %c./Sully_%%d%c, i);system(str);}}%c";if (strcmp(__FILENAME__, "Sully.c")){i--;}sprintf(str, "Sully_%d.c", i);fd = fopen(str, MODE);fprintf(fd,a,10,10,10,34,34,10,10,10,i,10,34,a,34,34,34,34,34,34,34,34,34,10);fclose(fd);char cmd[50];sprintf(cmd, "clang -Wall -Wextra -Werror Sully_%d.c -o Sully_%d", i, i);system(cmd);if(i > 0){sprintf(str, "./Sully_%d", i);system(str);}}
